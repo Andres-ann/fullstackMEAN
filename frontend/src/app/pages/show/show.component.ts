1e746d955e6795792d8e108a7c49f7db2ecb371d
Andres Negrette <andresdngtt@gmail.com>
2023-04-07 22:51:56 -0300

    routing ymistrar datos en la tabla

--- frontend/src/app/pages/show/show.component.ts
+++ frontend/src/app/pages/show/show.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  faCirclePlus,
+  faPen,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
+import { Product } from 'src/app/models/product.model';
+import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
-  styleUrls: ['./show.component.css']
+  styleUrls: ['./show.component.css'],
 })
-export class ShowComponent {
+export class ShowComponent implements OnInit {
+  faCirclePlus = faCirclePlus;
+  faPen = faPen;
+  faTrash = faTrash;
 
+  products: Product[] = [];
+
+  constructor(private crudeService: CrudService) {}
+
+  ngOnInit(): void {
+    this.crudeService.getProducts().subscribe((res: Product[]) => {
+      this.products = res;
+    });
+  }
+
+  delete(id: any, index: any) {
+    console.log(id);
+  }
 }
